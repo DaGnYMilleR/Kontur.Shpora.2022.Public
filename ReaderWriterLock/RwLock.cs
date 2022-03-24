2a8d41cd466f9daa48a340eff41829082205c16f
DaGnYMilleR <danil.lunyow@mail.ru>
2022-03-25 00:22:53 +0500

    done

---
+++ ReaderWriterLock/RwLock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ReaderWriterLock;
+
+public class RwLock : IRwLock
+{
+    private readonly object writeLock = new();
+    private volatile uint readers;
+
+    public void ReadLocked(Action action)
+    {
+        lock (writeLock)
+            Interlocked.Increment(ref readers);
+
+        try
+        {
+            action();
+        }
+        finally
+        {
+            Interlocked.Decrement(ref readers);
+        }
+    }
+
+    public void WriteLocked(Action action)
+    {
+        lock (writeLock)
+        {
+            while (readers > 0)
+            {
+            }
+
+            action();
+        }
+    }
+}
